09ba1f34daa442d677562c97b90bbc4d9358895f
bzrfp <btrz.rfp@gmail.com>
2026-08-30 19:41:55 -0300

    Create PersonalInfo.c

---
+++ w3schoolsProblemas/PersonalInfo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char name[100], city[100];
+    int age;
+    scanf("%s", name);
+    scanf("%d", &age);
+    scanf("%s", city);
+
+  	printf("Name: %s \n", name);
+  	printf("Age: %d \n", age);
+  	printf("City: %s \n", city);
+  
+  
+    return 0;
+}
